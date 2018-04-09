1e7ff696cf0974435baef7e0604e59bd305bf46b
jerrywang761208 <c1495616@gmail.com>
2018-04-09 11:28:45 +0800

    add chart to dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,59 @@
 <template>
   <div class="animated fadeIn">
     <p class="text-info">{{ $t("message.hello") }}</p>
+
+    <b-card-group columns class="card-columns cols-2">
+      <b-card header="Line Chart">
+        <div class="chart-wrapper">
+          <line-example/>
+        </div>
+      </b-card>
+      <b-card header="Bar Chart">
+        <div class="chart-wrapper">
+          <bar-example/>
+        </div>
+      </b-card>
+      <b-card header="Doughnut Chart">
+        <div class="chart-wrapper">
+          <doughnut-example/>
+        </div>
+      </b-card>
+      <b-card header="Radar Chart">
+        <div class="chart-wrapper">
+          <radar-example/>
+        </div>
+      </b-card>
+      <b-card header="瓦斯錶設備狀態">
+        <div class="chart-wrapper">
+          <pie-example/>
+        </div>
+      </b-card>
+      <b-card header="Polar Area Chart">
+        <div class="chart-wrapper">
+          <polar-area-example/>
+        </div>
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 
 <script>
+import BarExample from './dashboard/BarExample'
+import LineExample from './dashboard/LineExample'
+import DoughnutExample from './dashboard/DoughnutExample'
+import RadarExample from './dashboard/RadarExample'
+import PieExample from './dashboard/PieExample'
+import PolarAreaExample from './dashboard/PolarAreaExample'
+
 export default {
-  name: 'dashboard'
+  name: 'dashboard',
+  components: {
+    BarExample,
+    LineExample,
+    DoughnutExample,
+    RadarExample,
+    PieExample,
+    PolarAreaExample
+  }
 }
 </script>
---
+++ src/views/dashboard/PieExample.vue
@@ -0,0 +1,22 @@
+<script>
+import { Pie } from 'vue-chartjs'
+
+export default {
+  extends: Pie,
+  mounted () {
+    this.renderChart({
+      labels: ['Online', 'Warning', 'Error'],
+      datasets: [
+        {
+          backgroundColor: [
+            '#0BA7DA',
+            '#FFC200',
+            '#EA398C'
+          ],
+          data: [90, 10, 5]
+        }
+      ]
+    }, {responsive: true, maintainAspectRatio: false})
+  }
+}
+</script>
